9a8a47ba0956413b0d880d1550a39256785ad21a
David Chai <davidchaidev@gmail.com>
2019-08-25 12:05:52 -0400

    Time appended

--- client/components/RequestDisplay.jsx
+++ client/components/RequestDisplay.jsx
@@ -5,11 +5,14 @@ class RequestDisplay extends React.Component {
   render() {
     console.log(this.props);
     const {method, reqTime, elapsedTime, status} = this.props.reqInfo;
+    const parsedReqTime = new Date(reqTime).toLocaleTimeString();
+    const parsedElapsedTime = elapsedTime.toPrecision(1);
     return (
       <div className="request">
         <p>Method: {method}</p>
         <p>Status: {status}</p>
-        <p>Time It Took: </p>
+        <p>Request Time: {parsedReqTime}</p>
+        <p>Time It Took to Process: {parsedElapsedTime}ms</p>
       </div>
     )
   }
