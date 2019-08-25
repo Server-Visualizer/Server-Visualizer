6a645532a693dfa6ea5c2728d09fecb29a16fbd7
David Chai <davidchaidev@gmail.com>
2019-08-25 13:47:47 -0400

    Implemented SCSS styling

--- client/index.js
+++ client/index.js
@@ -1,5 +1,6 @@
 const React = require('react');
 const { render } = require('react-dom');
 import App from './App.jsx';
+import style from './style.scss'
 
 render(<App />, document.querySelector('#root'));
